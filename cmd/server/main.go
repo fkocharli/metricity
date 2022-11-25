f1246e4d45808e9e284a1ab70e6c0ac302368431
Farhad Kocharli <kocharli.farhad@gmail.com>
2022-11-25 19:27:40 +0400

    realized server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/fkocharli/metricity/internal/handlers"
+	"github.com/fkocharli/metricity/internal/server"
+	"github.com/fkocharli/metricity/internal/storage"
+)
+
+const (
+	address = "127.0.0.1:8080"
+)
+
+func main() {
+
+	repo := storage.NewRepository()
+	handler := handlers.NewHandler(repo)
+
+	mux := server.NewRouter(handler.Routes)
+
+	serv := server.New(address, mux)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	group := sync.WaitGroup{}
+
+	group.Add(1)
+	go func() {
+		defer group.Done()
+		if err := serv.Run(ctx); err != nil {
+			log.Printf("server run error: %v", err)
+			cancel()
+		}
+	}()
+
+	group.Wait()
+}
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,87 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/fkocharli/metricity/internal/server"
+	"github.com/fkocharli/metricity/internal/storage"
+)
+
+type ServerHandlers struct {
+	Routes     []server.Route
+	Repository storage.Repository
+}
+
+func NewHandler(r storage.Repository) ServerHandlers {
+
+	sh := ServerHandlers{
+		Repository: r,
+	}
+
+	sh.Routes = []server.Route{
+		server.Route{
+			Path:    "/update/gauge/",
+			Handler: http.HandlerFunc(sh.updateGauge),
+		},
+		server.Route{
+			Path:    "/update/counter/",
+			Handler: http.HandlerFunc(sh.updateCounter),
+		},
+		// server.Route{
+		// 	Path:    "/get",
+		// 	Handler: http.HandlerFunc(sh.get),
+		// },
+	}
+
+	return sh
+
+}
+
+func (s *ServerHandlers) updateGauge(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	n, m := getMetricsFromPath(r.URL.Path)
+
+	err := s.Repository.UpdateGaugeMetrics(n, m)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func (s *ServerHandlers) updateCounter(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	n, m := getMetricsFromPath(r.URL.Path)
+
+	err := s.Repository.UpdateCounterMetrics(n, m)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+// func (s *ServerHandlers) get(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte(s.Repository.Get()))
+// }
+
+func getMetricsFromPath(path string) (name, value string) {
+	metrics := strings.Split(path, "/")
+
+	name = metrics[3]
+	value = metrics[4]
+	return
+}
