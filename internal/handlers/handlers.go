8cb99581fa4320b6ecddb305cf3d7b7e072841e2
Farhad Kocharli <kocharli.farhad@gmail.com>
2022-11-28 19:22:56 +0400

    refactored handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -23,38 +23,39 @@ func NewHandler(r storage.Repository) ServerHandlers {
 
 	sh.Routes = []server.Route{
 		{
-			Path:    "/update/gauge/",
-			Handler: http.HandlerFunc(sh.updateGauge),
+			Path:    "/update/",
+			Handler: http.HandlerFunc(sh.update),
 		},
-		{
-			Path:    "/update/counter/",
-			Handler: http.HandlerFunc(sh.updateCounter),
-		},
-		// {
-		// 	Path:    "/get",
-		// 	Handler: http.HandlerFunc(sh.get),
-		// },
 	}
 
 	return sh
 
 }
 
-func (s *ServerHandlers) updateGauge(w http.ResponseWriter, r *http.Request) {
+func (s *ServerHandlers) update(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	n, m, er := valid(r.URL.Path, w)
+	x, n, m, er := valid(r.URL.Path, w)
 	if er != nil {
 		return
 	}
 
-	err := s.Repository.UpdateGaugeMetrics(n, m)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	switch x {
+	case "counter":
+		err := s.Repository.UpdateCounterMetrics(n, m)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	case "gauge":
+		err := s.Repository.UpdateGaugeMetrics(n, m)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.Header().Add("Content-Type", "text/plain")
@@ -62,48 +63,28 @@ func (s *ServerHandlers) updateGauge(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (s *ServerHandlers) updateCounter(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	n, m, er := valid(r.URL.Path, w)
-	if er != nil {
-		return
-	}
-
-	err := s.Repository.UpdateCounterMetrics(n, m)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Add("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-
-}
-
-// func (s *ServerHandlers) get(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte(s.Repository.Get()))
-// }
-
-func valid(s string, w http.ResponseWriter) (string, string, error) {
+func valid(s string, w http.ResponseWriter) (string, string, string, error) {
 	p := strings.Split(s, "/")
 	matched, err := regexp.MatchString(`/update/(gauge|counter)/[A-Za-z0-9]+/[0-9]`, s)
 	if err != nil || !matched {
+
 		if p[2] != "counter" && p[2] != "gauge" {
 			w.WriteHeader(http.StatusNotImplemented)
-			return "", "", err
+			return "", "", "", err
+		}
+
+		if len(p) < 5 {
+			w.WriteHeader(http.StatusNotFound)
+			return "", "", "", err
 		}
 
 		if _, err := strconv.Atoi(p[4]); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			return "", "", err
+			return "", "", "", err
 		}
 		w.WriteHeader(http.StatusNotFound)
-		return "", "", err
+		return "", "", "", err
 	}
 
-	return p[3], p[4], nil
+	return p[1], p[2], p[3], nil
 }
